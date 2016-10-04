fd9ca21e4524cfffe46331c2811827b53eeb0dd6
mr-kelley-mms <kelleyr@duvalschools.org>
2016-10-04 09:51:01 -0400

    Add files via upload
    This is basic code to help get you started.  You DO NOT have to code your program exactly like mine.

---
+++ go_fish-0.0a.py
@@ -0,0 +1,27 @@
+# Go Fish by Ryan Kelley - ICT-3
+# Version 0.0a - Published 10/04/2016
+
+from random import randint 
+from random import shuffle 
+# These lines import useful functions from the random module. 
+
+print("Welcome to Go Fish-bot Version 0.0a.  I am the premier Go Fish program in the universe.")
+user = input("Hello!  What is your name? [Type your name and press enter.] ")
+print("Hello,", user, "today we are going to play Go Fish!")
+# These lines introduce the user to our program and records their name.
+
+rules = int(input("Do you know how to play Go Fish? Enter 0 for NO and 1 for YES. "))
+
+if rules ___________: # Fix this IF statement to print the rules.
+    # Print the rules here. 
+else:
+    # Do something else if the player knows the rules. 
+
+deck = # Turn this variable into a LIST of the cards in the deck. 
+print(deck) # Make sure it worked.
+
+# How can we shuffle the deck?
+
+# This code should put you on the correct path to completing the project.
+
+
